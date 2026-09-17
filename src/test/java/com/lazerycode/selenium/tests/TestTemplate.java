c87ab65a0f3b46ceffcf5fd7c092e9db6d6c8d62
Sindhura <psindhura1496@gmail.com>
2026-09-17 16:41:12 +0530

    test1

--- src/test/java/com/lazerycode/selenium/tests/TestTemplate.java
+++ src/test/java/com/lazerycode/selenium/tests/TestTemplate.java
@@ -1,40 +1,46 @@
 package com.lazerycode.selenium.tests;
 
 import com.lazerycode.selenium.DriverBase;
-import org.testng.ITestContext;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.util.Properties;
 
-/**
- * Created by gvchaitanya on 09/11/17.
- */
-public class TestTemplate extends DriverBase{
-    public static Properties properties = new Properties();
-    String propertyFile = System.getProperty("myproperty");
-    String invFile = System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "java" + File.separator + "com"
-            + File.separator + "lazerycode" + File.separator + "selenium" + File.separator + "inv" + File.separator + propertyFile;
+public class TestTemplate extends DriverBase {
 
-    /**
-     * Loads the inventory file for the test
-     * @param context
-     * @throws Exception
-     */
+    public static Properties properties = new Properties();
+
+    String propertyFile = System.getProperty("myproperty");
+
+    String invFile = System.getProperty("user.dir")
+            + File.separator + "src"
+            + File.separator + "test"
+            + File.separator + "java"
+            + File.separator + "com"
+            + File.separator + "lazerycode"
+            + File.separator + "selenium"
+            + File.separator + "inv"
+            + File.separator + propertyFile;
+
     @BeforeSuite
-    public void beforeSuite(ITestContext context) throws Exception{
-       BufferedReader reader = new BufferedReader(new FileReader(invFile));
-       properties.load(reader);
-       System.out.println("WORKING!!" + properties.getProperty("url"));
+    public void beforeSuite() throws Exception {
+
+        BufferedReader reader = new BufferedReader(new FileReader(invFile));
+
+        properties.load(reader);
+
+        reader.close();
+
+        System.out.println("WORKING!! " + properties.getProperty("url"));
     }
 
     @AfterSuite
-    public void afterSuite(ITestContext context) throws Exception{
+    public void afterSuite() throws Exception {
+
         properties.store(new FileOutputStream(invFile), null);
     }
-
-
 }
